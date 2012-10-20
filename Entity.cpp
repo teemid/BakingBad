9d144d592ed5da401b541491db097521195540c1
Andreas Garmannslund <garmanns@stud.ntnu.no>
2012-10-20 18:13:53 +0200

    Entity glemte vi :P
    Signed-off-by: Andreas Garmannslund <garmanns@stud.ntnu.no>

--- Entity.cpp
+++ Entity.cpp
@@ -1,4 +1,6 @@
 #include "Entity.hpp"
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 
 bool Entity::Load( void )
 {
@@ -10,7 +12,8 @@ void Entity::Update( sf::Time delta )
 
 }
 
-void Entity::Draw( sf::Time delta )
+void Entity::Draw( sf::Time delta , sf::RenderWindow *window)
 {
     sprite.setTextureRect(sf::IntRect(0, 0, width, height)); // Foreløpig bare dette
+    window->draw(sprite);
 }
